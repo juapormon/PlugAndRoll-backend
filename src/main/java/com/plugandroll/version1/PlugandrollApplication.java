330b13155478d77ff952bc66f80b26e5e4df4989
Juan Pablo <juanp_portero9@hotmail.com>
2021-04-21 18:23:27 +0200

    initialized

---
+++ src/main/java/com/plugandroll/version1/PlugandrollApplication.java
@@ -0,0 +1,13 @@
+package com.plugandroll.version1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlugandrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlugandrollApplication.class, args);
+	}
+
+}
