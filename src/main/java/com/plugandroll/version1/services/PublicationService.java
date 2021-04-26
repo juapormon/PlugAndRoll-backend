af7bd110a2ad0b5152b86f392a17a4c43a4597d8
Juan Carlos <juancarlosjaen.jaeniglesias@gmail.com>
2021-04-26 20:59:14 +0200

    Añadido servicio de publicaciones

--- src/main/java/com/plugandroll/version1/services/PublicationService.java
+++ src/main/java/com/plugandroll/version1/services/PublicationService.java
@@ -1,2 +1,23 @@
-package com.plugandroll.version1.services;public class PublicationService {
+package com.plugandroll.version1.services;
+
+import com.plugandroll.version1.models.Publication;
+import com.plugandroll.version1.repositories.PublicationRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+
+@Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class PublicationService {
+
+    private PublicationRepository publicationRepository;
+
+    public List<Publication> findAll(){
+        List<Publication> res = publicationRepository.findAll();
+        Collections.reverse(res);
+        return res;
+    }
 }
