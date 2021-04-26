d398160c66d265a1b69b459392d36c8fe461acfd
Juan Carlos <juancarlosjaen.jaeniglesias@gmail.com>
2021-04-26 20:58:29 +0200

    Añadido modelo, el commit anterior era el controlador

--- src/main/java/com/plugandroll/version1/models/Publication.java
+++ src/main/java/com/plugandroll/version1/models/Publication.java
@@ -1,2 +1,28 @@
-package com.plugandroll.version1.models;public class Publication {
+package com.plugandroll.version1.models;
+
+
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "Publication")
+public class Publication extends  BaseEntity{
+
+    private String image;
+
+    @NotBlank
+    private String text;
+
+    @NotBlank
+    private LocalDateTime date;
+
+    @NotBlank
+    private UserEntity creator;
 }
