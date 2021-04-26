171385da0382931d9f67a40831a13f8e126ad156
Juan Carlos <juancarlosjaen.jaeniglesias@gmail.com>
2021-04-26 20:59:50 +0200

    Añadido Repositorio de Publicaciones

--- src/main/java/com/plugandroll/version1/repositories/PublicationRepository.java
+++ src/main/java/com/plugandroll/version1/repositories/PublicationRepository.java
@@ -1,2 +1,13 @@
-package com.plugandroll.version1.repositories;public interface PublicationRepository {
+package com.plugandroll.version1.repositories;
+
+import com.plugandroll.version1.models.Publication;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PublicationRepository extends MongoRepository<Publication, String> {
+
+    List<Publication> findAll();
 }
