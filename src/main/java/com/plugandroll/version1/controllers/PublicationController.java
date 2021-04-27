7598b07567f1064a727b5f36da0ab63bf9831d43
Juan Carlos <juancarlosjaen.jaeniglesias@gmail.com>
2021-04-27 18:06:48 +0200

    Agreagdos un servicio de obtencion por id y uno para crear en publicaciones

--- src/main/java/com/plugandroll/version1/controllers/PublicationController.java
+++ src/main/java/com/plugandroll/version1/controllers/PublicationController.java
@@ -4,12 +4,10 @@ import com.plugandroll.version1.models.Publication;
 import com.plugandroll.version1.services.PublicationService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +23,24 @@ public class PublicationController {
     public ResponseEntity<List<Publication>> findAll(){
         try{
             return ResponseEntity.ok(publicationService.findAll());
+        }catch (IllegalArgumentException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Publication> findById (@PathVariable final String id) throws NotFoundException {
+        try {
+            return ResponseEntity.ok(publicationService.findById(id));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<String> addPublication(@PathVariable final Publication publication){
+        try{
+            return ResponseEntity.status(HttpStatus.CREATED).body(publicationService.addPublication(publication));
         }catch (IllegalArgumentException e){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
--- src/main/java/com/plugandroll/version1/services/PublicationService.java
+++ src/main/java/com/plugandroll/version1/services/PublicationService.java
@@ -2,6 +2,7 @@ package com.plugandroll.version1.services;
 
 import com.plugandroll.version1.models.Publication;
 import com.plugandroll.version1.repositories.PublicationRepository;
+import io.jsonwebtoken.lang.Assert;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,5 +20,16 @@ public class PublicationService {
         List<Publication> res = publicationRepository.findAll();
         Collections.reverse(res);
         return res;
+    }
+
+    public Publication findById(String id){
+        List<Publication> list = publicationRepository.findAll();
+        Publication res = list.stream().filter(p -> p.getId().equals(id)).findAny().get();
+        return res;
+    }
+    public String addPublication(Publication publication){
+        Assert.notNull(publication);
+        this.publicationRepository.save(publication);
+        return "Added publication of: " + publication.getCreator().getUsername();
     }
 }
