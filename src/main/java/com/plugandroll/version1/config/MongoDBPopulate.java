b2329c93c981b7d0ca0f2b94a30d7de170d37b43
fragarroa <fragarroa@alum.us.es>
2021-06-17 16:50:31 +0200

    Wrong repo
    I was pushing all the changes to another repo. These are all the changes made since the last commit to this repo.

--- src/main/java/com/plugandroll/version1/config/MongoDBPopulate.java
+++ src/main/java/com/plugandroll/version1/config/MongoDBPopulate.java
@@ -2,6 +2,7 @@ package com.plugandroll.version1.config;
 
 import com.plugandroll.version1.models.TypeRol;
 import com.plugandroll.version1.models.UserEntity;
+import com.plugandroll.version1.repositories.RedBoxRepository;
 import com.plugandroll.version1.repositories.UserEntityRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -18,19 +19,27 @@ public class MongoDBPopulate<E> {
     PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(12);
 
     @Bean
-    CommandLineRunner commandLineRunner(UserEntityRepository userEntityRepository) {
+    CommandLineRunner commandLineRunner(UserEntityRepository userEntityRepository, RedBoxRepository redBoxRepository) {
         return strings -> {
             userEntityRepository.deleteAll();
+            redBoxRepository.deleteAll();
 
             /*================= USERS =================*/
 
-            UserEntity master = new UserEntity("asdf",
+            UserEntity asdf = new UserEntity("asdf",
                     passwordEncoder.encode("asdfasdf"),
                     "asdf@asdf.com",
                     true,
-                    Stream.of(TypeRol.ADMIN, TypeRol.DM).collect(Collectors.toSet()));
+                    Stream.of(TypeRol.ADMIN, TypeRol.DM, TypeRol.PLAYER).collect(Collectors.toSet()));
 
-            userEntityRepository.save(master);
+            UserEntity fdsa = new UserEntity("fdsa",
+                    passwordEncoder.encode("asdfasdf"),
+                    "fdsa@fdsa.com",
+                    true,
+                    Stream.of(TypeRol.DM, TypeRol.PLAYER).collect(Collectors.toSet()));
+
+            userEntityRepository.save(asdf);
+            userEntityRepository.save(fdsa);
 
         };
     }
